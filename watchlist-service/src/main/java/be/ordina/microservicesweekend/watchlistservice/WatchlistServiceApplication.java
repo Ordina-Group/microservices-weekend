428b8dd6bbf681f35d7fe8e90309a5b0d60d42f8
Dieter Hubau <dhubau@gmail.com>
2019-02-25 16:17:13 +0100

    Fixed some issues for default running locally

--- watchlist-service/src/main/java/be/ordina/microservicesweekend/watchlistservice/WatchlistServiceApplication.java
+++ watchlist-service/src/main/java/be/ordina/microservicesweekend/watchlistservice/WatchlistServiceApplication.java
@@ -2,13 +2,15 @@ package be.ordina.microservicesweekend.watchlistservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WatchlistServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WatchlistServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WatchlistServiceApplication.class, args);
+    }
 }
